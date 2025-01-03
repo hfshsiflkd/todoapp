6edbff7c2352f4c87357cd923561eaa45e566004
hfshsiflkd <toogiit0521@gmail.com>
2025-01-03 11:28:34 +0800

    js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
-
+import React, { useState } from "react";
 function App() {
+  const [todo, setTodos] = useState([]);
+  const [error, setError] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const handleInput = (evnet) => {
+    setInputValue(evnet.target.value);
+  };
+  const handleAdd = () => {
+    if (inputValue === "") {
+      setError("Please enter a value");
+    } else {
+      setTodos([...todo, inputValue]);
+      setInputValue("");
+      setError("");
+    }
+  };
+
   return (
     <div className="App">
       <div className="App-header">
@@ -10,8 +26,13 @@ function App() {
               <h1>To-Do list</h1>
             </div>
             <div class="beak">
-              <input></input>
-              <button>ADD</button>
+              <input
+                placeholder="Add to do"
+                value={inputValue}
+                onChange={handleInput}
+              />
+              {error.length > 1 && <div>{error}</div>}
+              <button onClick={handleAdd}>ADD</button>
             </div>
             <div class="hair">
               <div class="All">All</div>
@@ -19,18 +40,15 @@ function App() {
               <div class="Completed">Completed</div>
             </div>
           </div>
-          <div class="feet">
-          No tasks yet. Add one above!
-          </div>
+          <div class="feet">{todo.map((todo) => {
+                return <div>{todo}</div>;
+              })}</div>
+          
         </div>
-        <div class="foot">
-        Powered by
-        Pinecone academy
-        </div>
+        <div class="foot">Powered by Pinecone academy</div>
       </div>
     </div>
   );
 }
-
 
 export default App;
