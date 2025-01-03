4a7b2c32d2c0db007e366497750ab3e420923006
hfshsiflkd <toogiit0521@gmail.com>
2025-01-03 10:36:22 +0800

    lag
    lag2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <div className="App-header">
+        <div class="main">
+          <div class="head">
+            <div class="eyes">
+              <h1>To-Do list</h1>
+            </div>
+            <div class="beak">
+              <input></input>
+              <button>ADD</button>
+            </div>
+            <div class="hair">
+              <div class="All">All</div>
+              <div class="Active">Active</div>
+              <div class="Completed">Completed</div>
+            </div>
+          </div>
+          <div class="feet">
+          No tasks yet. Add one above!
+          </div>
+        </div>
+        <div class="foot">
+        Powered by
+        Pinecone academy
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
