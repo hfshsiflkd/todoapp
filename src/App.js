5507ac8bfe621c7cb0d3823c11aef433a07f2e23
hfshsiflkd <toogiit0521@gmail.com>
2025-01-06 11:52:26 +0800

    bugs
    bug

--- src/App.js
+++ src/App.js
@@ -5,41 +5,58 @@ function App() {
   const [todo, setTodos] = useState([]);
   const [error, setError] = useState("");
   const [inputValue, setInputValue] = useState("");
-  const [filter, setFilter] = useState("All");
+  const [filterstate, setFilterstate] = useState("All");
+
+
   const handleInput = (event) => {
     setInputValue(event.target.value);
   };
+  const handlefilterstate = (filterstate) => {
+    setFilterstate(filterstate);
+  };
   const handleAdd = () => {
+    console.log(todo);
+    
     if (inputValue === "") {
       alert("Please enter a value");
     } else {
-      setTodos([...todo, inputValue]);
+      setTodos([...todo, {text: inputValue, id: Date.now(), status: "Active"}]);
       setInputValue("");
       setError("");
     }
-    
   };
   const handlebox = (id) => {
-    const newTodo = todo.map((todo, index) => {
-      if (index === id) {
-        return { ...todo, status: "DONE" };
-      }else {
+    const newTodo = todo.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, status: "Completed" };
+      } else {
         return todo;
       }
-      
     });
-    setTodos(newTodo);
-  }
+  };
 
+
+
+  console.log(filterstate);
+  const ll = todo
+  .filter((todo) => {
+    if (filterstate === "All") {
+      return true;
+    } else {
+      return todo.status === filterstate;
+    }
+  })
+  console.log(ll);
+  
   return (
     <div className="App">
       <div className="App-header">
-        <div class="main">
-          <div class="head">
-            <div class="eyes">
+        <div className="main">
+          <div className="head">
+            <div className="eyes">
               <h1>To-Do list</h1>
             </div>
-            <div class="beak">
+            <div className="beak">
               <input
                 placeholder="Add to do"
                 value={inputValue}
@@ -47,19 +64,41 @@ function App() {
               />
               <button onClick={handleAdd}>ADD</button>
             </div>
-            <div class="hair">
-              <div class="All" onClick={() => filter} >All</div>
-              <div class="Active">Active</div>
-              <div class="Completed">Completed</div>
+            <div className="hair">
+              <div className="All" onClick={() => handlefilterstate("All")}>
+                All
+              </div>
+              <div
+                className="Active"
+                onClick={() => handlefilterstate("Active")}
+                style={{ color: "red" }}
+              >
+                Active
+              </div>
+              <div
+                className="Completed"
+                onClick={() => handlefilterstate("Completed")}
+              >
+                Completed
+              </div>
             </div>
-          </div>
-          <div class="feet" >
-            {todo.map((todo) => {
-              return <div>{todo}</div>;
-            })}
+            {todo
+              .filter((todo) => {
+                if (filterstate === "All") {
+                  return true;
+                } else {
+                  return todo.status === filterstate;
+                }
+              })
+              .map((todo) => (
+                <div key={todo} className="todo">
+                  <input className="todotext1" type="checkbox"  onClick={() => handlebox(todo.id)} />
+                  <p className="todotext">{todo.text}</p>
+                </div>
+              ))}
           </div>
         </div>
-        <div class="foot">Powered by Pinecone academy</div>
+        <div className="foot">Powered by Pinecone academy</div>
       </div>
     </div>
   );
